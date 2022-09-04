3befd1b4c58d6e25e7d4cd40e0b2b4f53b2fb716
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-09-04 12:15:06 -0400

    Create number_spiral.cpp

---
+++ Introductory Problems/number_spiral.cpp
@@ -0,0 +1,43 @@
+// code by savir singh
+
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    int t;
+    ll y, x;
+    cin >> t;
+    for (int s=0; s<t; s++)
+    {
+        cin >> y >> x;
+        if (y < x)
+        {
+            if (x % 2 != 0)
+            {
+                ll num = x * x;
+                cout << num - y + 1 << "\n";
+            }
+            else
+            {
+                ll num = (x - 1) * (x - 1) + 1;
+                cout << num + y - 1 << "\n";
+            }
+        }
+        else
+        {
+            if (y % 2 == 0)
+            {
+                ll num = y * y;
+                cout << num - x + 1 << "\n";
+            }
+            else
+            {
+                ll num = (y - 1) * (y - 1) + 1;
+                cout << num + x - 1 << "\n";
+            }
+        }
+    }
+    return 0;
+}
